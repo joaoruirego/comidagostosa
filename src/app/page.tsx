39885f58cba1e0fae3cdc2855317e941326dd704
step-github <geral@meetstep.com>
2025-05-31 23:49:31 +0100

    style landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import CategorySelection from "./components/CategorySelection";
 import styles from "./styles/HomePage.module.css";
+
 const HomePage: React.FC = () => {
   return (
     <div className={styles.container}>
