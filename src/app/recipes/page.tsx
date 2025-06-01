aa0f5e435605ccfba3aaa1b5b1c23d3b8e8c7ca6
step-github <geral@meetstep.com>
2025-06-01 15:35:21 +0100

    ta build with sucess

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import RecipeList from "../components/RecipeList";
 
 const RecipesPage: React.FC = () => {
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <RecipeList />
-    </div>
+    </Suspense>
   );
 };
 
